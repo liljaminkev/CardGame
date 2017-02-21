79cf59854f4a307abb03921838d8e702308643be
Kevin <kevchan85@gmail.com>
2017-02-20 22:37:06 -0800

    Brought forward some missing files

--- src/strategyassets/Strategy.java
+++ src/strategyassets/Strategy.java
@@ -1,10 +1,13 @@
+//Strategy is an interface to get legal moves when placing, or attacking the enemy player
 package strategyassets;
 
-
 import playerassets.Player;
-
+import moveassets.Move;
 //interface that returns a move
-public interface Strategy
+public abstract class Strategy
 {
-    public int getLegalMove(Player playerA);
+
+    public Move getLegalMove(Player playerA){return null;}
+    public Move getLegalMove(Player playerA, Player playerB){return null;}
+
 }
